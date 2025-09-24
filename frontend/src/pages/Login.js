d8cfcd6eaaec377cb85591a104a7bb373673fdd4
Malek Sakir <sakirm@infopercept.com>
2025-09-24 13:24:06 +0530

    fixed indentation issue

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
